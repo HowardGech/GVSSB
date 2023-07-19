b3549c8e7d3a0107f9a74372041d193846ba52d3
Changhao Ge <76844389+HowardGech@users.noreply.github.com>
2023-07-19 18:11:32 -0400

    Add files via upload

--- R/predict.GVSSB.R
+++ R/predict.GVSSB.R
@@ -9,18 +9,18 @@
 #' \tabular{ll}{
 #'    \code{Y_hat} \tab The estimated value of the response vector. \cr
 #' }
-#' @seealso \code{\link{GVSSB}}, \code{\link{cv.GVSSB}} 
+#' @seealso \code{\link{GVSSB}}, \code{\link{cv.GVSSB}}
 #' @examples
 #' # load GVSSB library
 #' library(GVSSB)
-#' 
+#'
 #' # generate covariates
 #' n <- 200
 #' G <- 200
 #' p_i <- 5
 #' p <- G * p_i
 #' X <- mvtnorm::rmvnorm(n, sigma=diag(p))
-#' 
+#'
 #' # generate coefficients
 #' k <- 10
 #' beta <- rep(0,p)
@@ -28,34 +28,40 @@
 #' for(index in nonzero_group){
 #'     beta[p_i * (index - 1) + 1:p_i] <- runif(p_i, -1, 1)
 #' }
-#' 
+#'
 #' # define group index
 #' groups <- rep(1:G, each = p_i)
-#' 
+#'
 #' # generate response vector
 #' Y <- X %*% beta + rnorm(n, 0, 1)
-#' 
+#'
 #' # fit GVSSB model
 #' fit.Gaussian <- GVSSB(X, Y, groups, prior = 'Gaussian')
 #' fit.Laplace <- GVSSB(X, Y, groups, prior = 'Laplace')
 #' fit.Cauchy <- GVSSB(X, Y, groups, prior = 'T', nu = 1)
-#' 
-#' # 
+#'
+#' # give the prediction
+#' n_test <- 50
+#' X_test <- mvtnorm::rmvnorm(n_test, sigma=diag(p))
+#' predict.Gaussian <- predict.GVSSB(fit.Gaussian, X_test)
+#' predict.Laplace <- predict.GVSSB(fit.Laplace, X_test)
+#' predict.Cauchy <- predict.GVSSB(fit.Cauchy, X_test)
+#'
 #' @export predict.GVSSB
 #' @export
 predict.GVSSB = function(object, X, threshold, ...){
-  
+
   # consistency check
   if(!is.matrix(X)) stop("X should be a matrix!")
   n = nrow(X)
   p = ncol(X)
   if(p != length(object$groups_index)) stop("The dimension of X doesn't match!")
-  
+
   # get the coefficients estimated by the GVSSB model
   beta = object$beta
-  
+
   intercept = object$intercept
-  
+
   # compute the prediction of the response vector
   Y_hat = X %*% beta + intercept
   fit = list(Y_hat = Y_hat)
